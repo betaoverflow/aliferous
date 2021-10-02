96746e668508543a2bc4f3a6037318fb8faca356
Aniket762 <aniketindian8@gmail.com>
2021-10-03 00:39:33 +0530

    Feat: 3d characters and terrain added

---
+++ Assets/Buttons/WebURLScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebURLScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void btnOne()
+    {
+        Application.OpenURL("https://aniket762.github.io/automatic-happiness/");
+    }
+
+    public void btnTwo()
+    {
+        Application.OpenURL("https://sumana2001.github.io/aliferous-puzzle/index.html");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
