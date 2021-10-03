c7fbc6258585b10c750a31548b865b36c597428e
Aniket762 <aniketindian8@gmail.com>
2021-10-03 12:16:58 +0530

    Feat: Quiz application added

---
+++ app/index.js
@@ -0,0 +1,248 @@
+//import elements
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
+const result_box = document.querySelector(".result_box");
+const option_list = document.querySelector(".option_list");
+const time_line = document.querySelector("header .time_line");
+const timeText = document.querySelector(".timer .time_left_txt");
+const timeCount = document.querySelector(".timer .timer_sec");
+const restart = result_box.querySelector(".buttons .restart");
+const game = result_box.querySelector(".buttons .game");
+
+// Start
+start_btn.onclick = () => {
+  info_box.classList.add("activeInfo");
+};
+
+// Exit
+exit_btn.onclick = () => {
+  info_box.classList.remove("activeInfo");
+};
+
+// Continue
+continue_btn.onclick = () => {
+  info_box.classList.remove("activeInfo");
+  quiz_box.classList.add("activeQuiz");
+  showQuetions(0);
+  queCounter(1);
+  startTimer(15);
+  startTimerLine(0);
+};
+
+let timeValue = 15;
+let que_count = 0;
+let que_numb = 1;
+let userScore = 0;
+let counter;
+let counterLine;
+let widthValue = 0;
+
+const restart_quiz = result_box.querySelector(".buttons .restart");
+const quit_quiz = result_box.querySelector(".buttons .quit");
+
+// Restart
+restart_quiz.onclick = () => {
+  quiz_box.classList.add("activeQuiz");
+  result_box.classList.remove("activeResult");
+  timeValue = 15;
+  que_count = 0;
+  que_numb = 1;
+  userScore = 0;
+  widthValue = 0;
+  showQuetions(que_count);
+  queCounter(que_numb);
+  clearInterval(counter);
+  clearInterval(counterLine);
+  startTimer(timeValue);
+  startTimerLine(widthValue);
+  timeText.textContent = "Time Left";
+  next_btn.classList.remove("show");
+};
+
+// Quit
+quit_quiz.onclick = () => {
+  window.location.reload(); //reload
+};
+
+const next_btn = document.querySelector("footer .next_btn");
+const bottom_ques_counter = document.querySelector("footer .total_que");
+
+// Next
+next_btn.onclick = () => {
+  if (que_count < questions.length - 1) {
+    que_count++;
+    que_numb++;
+    showQuetions(que_count);
+    queCounter(que_numb);
+    clearInterval(counterLine);
+    startTimer(timeValue);
+    startTimerLine(widthValue);
+    timeText.textContent = "Time Left";
+    next_btn.classList.remove("show");
+  } else {
+    clearInterval(counter);
+    clearInterval(counterLine);
+    showResult(); // Show Results
+  }
+};
+
+// Get Questions
+function showQuetions(index) {
+  const que_text = document.querySelector(".que_text");
+
+  let que_tag =
+    "<span>" +
+    questions[index].numb +
+    ". " +
+    questions[index].question +
+    "</span>";
+  let option_tag =
+    '<div class="option"><span>' +
+    questions[index].options[0] +
+    "</span></div>" +
+    '<div class="option"><span>' +
+    questions[index].options[1] +
+    "</span></div>" +
+    '<div class="option"><span>' +
+    questions[index].options[2] +
+    "</span></div>" +
+    '<div class="option"><span>' +
+    questions[index].options[3] +
+    "</span></div>";
+  que_text.innerHTML = que_tag;
+  option_list.innerHTML = option_tag;
+
+  const option = option_list.querySelectorAll(".option");
+
+  // onclick attribute
+  for (i = 0; i < option.length; i++) {
+    option[i].setAttribute("onclick", "optionSelected(this)");
+  }
+}
+// icons
+let tickIconTag = '<div class="icon tick"><i class="fas fa-check"></i></div>';
+let crossIconTag = '<div class="icon cross"><i class="fas fa-times"></i></div>';
+
+// option selected
+function optionSelected(answer) {
+  clearInterval(counter);
+  clearInterval(counterLine);
+  let userAns = answer.textContent;
+  let correcAns = questions[que_count].answer; //get correct answer
+  const allOptions = option_list.children.length; //get all options
+
+  if (userAns == correcAns) {
+    //if correct answer
+    userScore += 1;
+    answer.classList.add("correct"); //add green color
+    answer.insertAdjacentHTML("beforeend", tickIconTag); //add tick icon
+    console.log("Correct Answer");
+    console.log("Your correct answers = " + userScore);
+  } else {
+    answer.classList.add("incorrect"); //add red color
+    answer.insertAdjacentHTML("beforeend", crossIconTag); //add cross icon
+    console.log("Wrong Answer");
+
+    for (i = 0; i < allOptions; i++) {
+      if (option_list.children[i].textContent == correcAns) {
+        option_list.children[i].setAttribute("class", "option correct");
+        option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag);
+        console.log("Auto selected correct answer.");
+      }
+    }
+  }
+  for (i = 0; i < allOptions; i++) {
+    option_list.children[i].classList.add("disabled"); //disable all options
+  }
+  next_btn.classList.add("show");
+}
+
+function showResult() {
+  info_box.classList.remove("activeInfo");
+  quiz_box.classList.remove("activeQuiz");
+  result_box.classList.add("activeResult");
+  const scoreText = result_box.querySelector(".score_text");
+  if (userScore > 6) {
+    let scoreTag =
+      "<span>And congrats 🎉, You got <p>" +
+      userScore +
+      "</p> out of <p>" +
+      questions.length +
+      "</p></span>";
+    scoreText.innerHTML = scoreTag;
+    restart.style.display = "none";
+  } else if (userScore > 4 && userScore <= 6) {
+    let scoreTag =
+      "<span>And nice 😎, You got <p>" +
+      userScore +
+      "</p> out of <p>" +
+      questions.length +
+      "</p></span>";
+    scoreText.innerHTML = scoreTag;
+    game.style.display = "none";
+  } else {
+    let scoreTag =
+      "<span>And sorry 😐, You got only <p>" +
+      userScore +
+      "</p> out of <p>" +
+      questions.length +
+      "</p></span>";
+    scoreText.innerHTML = scoreTag;
+    game.style.display = "none";
+  }
+}
+
+function startTimer(time) {
+  counter = setInterval(timer, 1000);
+  function timer() {
+    timeCount.textContent = time;
+    time--;
+    if (time < 9) {
+      let addZero = timeCount.textContent;
+      timeCount.textContent = "0" + addZero;
+    }
+    if (time < 0) {
+      //if question unattemptted
+      clearInterval(counter);
+      timeText.textContent = "Time Off";
+      const allOptions = option_list.children.length; //get all options
+      let correcAns = questions[que_count].answer; //get correct answer
+      for (i = 0; i < allOptions; i++) {
+        if (option_list.children[i].textContent == correcAns) {
+          option_list.children[i].setAttribute("class", "option correct"); //add green color
+          option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); //add tick icon
+          console.log("Time Off: Auto selected correct answer.");
+        }
+      }
+      for (i = 0; i < allOptions; i++) {
+        option_list.children[i].classList.add("disabled");
+      }
+      next_btn.classList.add("show");
+    }
+  }
+}
+
+function startTimerLine(time) {
+  counterLine = setInterval(timer, 29);
+  function timer() {
+    time += 1;
+    time_line.style.width = time + "px";
+    if (time > 549) {
+      clearInterval(counterLine);
+    }
+  }
+}
+
+function queCounter(index) {
+  let totalQueCounTag =
+    "<span><p>" +
+    index +
+    "</p> of <p>" +
+    questions.length +
+    "</p> Questions</span>";
+  bottom_ques_counter.innerHTML = totalQueCounTag;
+}
+s;
---
+++ app/questions.js
@@ -0,0 +1,84 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "Cube root of 1331 is:",
+    answer: "11",
+    options: ["11", "13", "12", "17"],
+  },
+  {
+    numb: 2,
+    question: "The value of pi is equal to:",
+    answer: "22/7",
+    options: ["24/7", "22/7", "23/7", "22/8"],
+  },
+  {
+    numb: 3,
+    question: "Which is the most sensitive organ in our body?",
+    answer: "Skin",
+    options: ["Skin", "Heart", "Brain", "Stomach"],
+  },
+  {
+    numb: 4,
+    question: "The circumference of the circle is also sometimes called:",
+    answer: "Perimeter of a circle",
+    options: [
+      "Arc of a circle",
+      "Area of a circle",
+      "Perimeter of a circle",
+      "Diameter of a circle",
+    ],
+  },
+  {
+    numb: 5,
+    question: "Who invented Computer?",
+    answer: "Charles Babbage",
+    options: [
+      "Bill Gates",
+      "Charles Babbage",
+      "Alexander Cumming",
+      "Mark Zuckerberg",
+    ],
+  },
+
+  {
+    numb: 6,
+    question: "The area of rectangle is equal to:",
+    answer: "Length x Breadth",
+    options: [
+      "Length x Breadth",
+      "2 x (Length x Breadth)",
+      "2 x (Length - Breadth)",
+      "½ x base x height ",
+    ],
+  },
+  {
+    numb: 7,
+    question: "The square root of 5 is",
+    answer: "2.23",
+    options: ["2.24", "2.23", "2.50", "2.89"],
+  },
+  {
+    numb: 8,
+    question: "Who is known as Father of Indian Constitution? ",
+    answer: "Dr. B. R. Ambedkar",
+    options: [
+      "Mahatama Gandhi",
+      "Jawaharlal Nehru",
+      "Dr. B. R. Ambedkar",
+      "Subash Chandra Bose",
+    ],
+  },
+  {
+    numb: 9,
+    question: "1024 Kilobytes is equal to?",
+    answer: "1 MB",
+    options: ["10 MB", "1 TB", "1 MB", "1 GB"],
+  },
+  {
+    numb: 10,
+    question: "8 raised to power 0 is:",
+    answer: "1",
+    options: ["1", "0", "8", "Undefined"],
+  },
+];
